8954a2f647bf8e1edaec1862712b5253d55af93a
MegaEmme <mikmusco@gmail.com>
2025-04-09 15:05:26 +0200

    feat: Milestone 2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,39 @@
 import { useState } from "react";
-import articles from "../data/articles";
 
 function Main() {
+
+    const [article, setArticle] = useState([
+        "The Legend of Zelda: Breath of the Wild",
+        "Red Dead Redemption 2",
+        "The Last of Us Part II",
+        "Super Mario Odyssey",
+        "Minecraft"
+    ]);
     const [newArticle, setNewArticle] = useState('');
+
+    const addArticle = event => {
+        event.preventDefault();
+        const updatedArticles = [...article, newArticle];
+        setArticle(updatedArticles);
+        console.log('Aggiunto: ' + newArticle);
+    }
+
     return (
         <>
-            <main className="container">
-                <ul>
-                    {articles.map(element =>
-                        <li key={element.id}>{element.title}</li>
-                    )}
-                </ul>
-            </main>
+            <form onSubmit={addArticle}>
+                <main className="container">
+                    <ul>
+                        {article.map((element, index) =>
+                            <li key={index}>{element}</li>
+                        )}
+                    </ul>
+                    <input type="text"
+                        onChange={e => { setNewArticle(e.target.value) }}
+                    />
+                    <hr />
+                    <button className="btn btn-primary">Invia</button>
+                </main>
+            </form>
         </>
     )
 }
