5c5bc776d51b40b427536c252bb00e0ad29b937d
MegaEmme <mikmusco@gmail.com>
2025-04-09 16:05:04 +0200

    feat: bonus 1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,20 +20,31 @@ function Main() {
         console.log('Aggiunto: ' + newArticle);
     }
 
+    const removeArticle = i => {
+        const deletedArticles = article.filter((article, index) => {
+            return index !== i
+        })
+        setArticle(deletedArticles);
+    }
+
+
     return (
         <>
             <form onSubmit={addArticle}>
                 <main className="container">
                     <ul>
                         {article.map((element, index) =>
-                            <li key={index}>{element}</li>
+                            <li className="p-3" key={index}>{element}
+                                <hr />
+                                <button className="btn btn-danger" onClick={() => removeArticle(index)}><i className="fa-regular fa-trash-can"></i></button></li>
                         )}
                     </ul>
                     <input type="text"
                         onChange={e => { setNewArticle(e.target.value) }}
+                        placeholder="Inserisci nuovo gioco"
                     />
                     <button className="btn btn-primary mx-4"><i className="fa-solid fa-plus"></i></button>
-                    <button className="btn btn-danger"><i className="fa-regular fa-trash-can"></i></button>
+
                 </main>
             </form>
         </>
