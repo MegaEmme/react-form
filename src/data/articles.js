4ad25f9d6b67eb3e5cf99c27ddf2a52f02e972f3
MegaEmme <mikmusco@gmail.com>
2025-04-09 13:39:42 +0200

    feat: add key:id to array

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,24 +1,29 @@
 const articles = [
+  {
+    id: 1,
+    title: "The Legend of Zelda: Breath of the Wild",
+    description: "Un'avventura epica in un mondo aperto, dove il giocatore esplora Hyrule e combatte contro Calamity Ganon."
+  },
+  {
+    id: 2,
+    title: "Red Dead Redemption 2",
+    description: "Un western open-world con una storia coinvolgente e un mondo dettagliato da esplorare."
+  },
+  {
+    id: 3,
+    title: "The Witcher 3: Wild Hunt",
+    description: "Un gioco di ruolo d'azione con una trama complessa, scelte morali difficili e un vasto mondo fantasy."
+  },
     {
-      title: "The Legend of Zelda: Breath of the Wild",
-      description: "Un'avventura epica in un mondo aperto, dove il giocatore esplora Hyrule e combatte contro Calamity Ganon."
-    },
-    {
-      title: "Red Dead Redemption 2",
-      description: "Un western open-world con una storia coinvolgente e un mondo dettagliato da esplorare."
-    },
-    {
-      title: "The Witcher 3: Wild Hunt",
-      description: "Un gioco di ruolo d'azione con una trama complessa, scelte morali difficili e un vasto mondo fantasy."
-    },
-      {
-      title: "Super Mario Odyssey",
-      description: "Un platform 3D colorato e creativo, dove Mario viaggia in diversi regni per salvare la Principessa Peach."
-    },
-     {
-      title: "Elden Ring",
-      description: "Un gioco di ruolo d'azione open-world con un alto livello di difficoltà e un mondo oscuro e affascinante."
-    }
-  ];
+    id: 4,
+    title: "Super Mario Odyssey",
+    description: "Un platform 3D colorato e creativo, dove Mario viaggia in diversi regni per salvare la Principessa Peach."
+  },
+   {
+    id: 5,
+    title: "Elden Ring",
+    description: "Un gioco di ruolo d'azione open-world con un alto livello di difficoltà e un mondo oscuro e affascinante."
+  }
+];
 
   export default articles;
